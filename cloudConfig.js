81373c69349c76940885f95eabbdf7736b8885c8
ShashiSacks <shashidharreddy.mopuri@gmail.com>
2026-05-13 15:10:15 +0530

    Final cloudinary fix

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,28 +1,51 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary =
+  require("cloudinary").v2;
 
-const {
-  CloudinaryStorage,
-} = require("multer-storage-cloudinary");
+const CloudinaryStorage =
+  require("multer-storage-cloudinary")
+    .CloudinaryStorage;
+
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+
+  cloud_name:
+    process.env.CLOUD_NAME,
+
+  api_key:
+    process.env.CLOUD_API_KEY,
+
+  api_secret:
+    process.env.CLOUD_API_SECRET,
+
 });
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
 
-  params: async (req, file) => {
-    return {
-      folder: "Wanderlust_DEV",
+const storage =
+  new CloudinaryStorage({
 
-      format: file.mimetype.split("/")[1],
-    };
-  },
-});
+    cloudinary,
+
+    params: async (req, file) => {
+
+      return {
+
+        folder:
+          "Wanderlust_DEV",
+
+        format:
+          file.mimetype.split("/")[1],
+
+      };
+
+    },
+
+  });
+
 
 module.exports = {
+
   cloudinary,
+
   storage,
+
 };
